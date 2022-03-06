73a129ae6b1668151b409db6df5023984a08d107
ServerToMbak <server78135@gmail.com>
2022-03-06 15:17:50 +0300

    Basic changes

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -3,31 +3,32 @@ package oopIntro;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product1=new Product(1,"lenova v14",15000,"16gb ram");
+		Product product1=new Product(1,"lenova v14",15000,"16gb ram",10);
 		
 		Product product2=new Product();
-		product2.id=2;
-		product2.name="lenova V 15";
-		product2.unitPrice=16000;
-		product2.detail="32 gb ram";
-		
+		product2.setId(1);
+		product2.setName("lenova V 15");
+		product2.setDetail("16 gb ram");
+		product2.setDisCount(10);
+		product2.setUnitPrice(16000);
+
 		Product product3=new Product();
-		product3.id=3;
-		product3.name="hp 5";
-		product3.unitPrice=10000;
-		product3.detail="8 gb ram";
+		
 		
 		Product[] products = {product1,product2,product3};
 		
 		for (Product product : products) {
-			System.out.println(product.name);
+			System.out.println(product.getName()+product.getUnitPriceAfterDiscount());
 		}
+		
+		
 		Category category1=new Category();
-		category1.id=1;
-		category1.name="bilgisayar";
+		category1.setId(1);;
+		category1.setName("içecek");
+		
 		Category category2=new Category();
-		category2.id=2;
-		category2.name="Ev/Bahçe";
+		category2.setId(2);;
+		category2.setName("yiyecek");
 		System.out.println(products.length);
 		
 		
--- oopIntro/src/oopIntro/ProductManager.java
+++ oopIntro/src/oopIntro/ProductManager.java
@@ -2,6 +2,6 @@ package oopIntro;
 
 public class ProductManager {
 		public void addToCart(Product product) {
-			System.out.println("sepete eklendi " +product.name);
+			System.out.println("sepete eklendi " +product.getName());
 		}
 }
