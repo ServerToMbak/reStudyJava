dbcae2bc46cdf3b171ce7d87f5cda20793e95304
ServerToMbak <server78135@gmail.com>
2022-04-04 21:44:30 +0300

    FirstHibernateDemo

---
+++ hibernateDemo/src/com/hibernatedemo/City.java
@@ -0,0 +1,57 @@
+package com.hibernatedemo;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="city")
+public class City {
+	@Id
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="Name")
+	private String name;
+	
+	@Column(name="CountryCode")
+	private String CountryCode;
+	
+	@Column(name="District")
+	private String district;
+	
+	@Column(name="Population")
+	private int population;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountryCode() {
+		return CountryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.CountryCode = countryCode;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	
+
+}
---
+++ hibernateDemo/src/com/hibernatedemo/Main.java
@@ -0,0 +1,68 @@
+package com.hibernatedemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();		
+		Session session = factory.getCurrentSession();
+		try {
+			session.beginTransaction();
+			//from City c ORDER BY c.name DESC
+			//HQL --> hibernate query language
+			//from City c where c.CountryCode='TUR' AND c.district='Ankara' //sql sorgusu
+			//from City c where c.name like '%kar%'
+			//from City c ORDER BY c.name DESC
+		
+			//-----select iþlemi------
+			/*
+			 * List<String> countryCodes = session.
+			 * createQuery("SELECT c.CountryCode from City c GROUP BY c.CountryCode ").
+			 * getResultList();
+			 * 
+			 * for(String countryCode:countryCodes) { System.out.println(countryCode); }
+			 */
+		
+			//------Ýnsert iþlemi----------
+			
+			/*
+			 * City city= new City(); city.setName("düzce 10"); city.setCountryCode("TUR");
+			 * city.setDistrict("Karadeniz"); city.setPopulation(100000);
+			 * 
+			 * session.save(city); session.getTransaction().commit();
+			 * System.out.println("ÞEHÝR EKLENDÝ ");
+			 */
+			
+			//-----UPDATE ÝÞLEMÝ----------
+			/*
+			 * City city =session.get(City.class,4090); city.setPopulation(110000);
+			 * 
+			 * session.save(city);
+			 * 
+			 * 
+			 * session.getTransaction().commit(); System.out.println("ÞEHÝR GÜNCELLENDÝ");
+			 */
+			
+			
+			//-----DELETE ÝÞLEMÝ-----
+			  
+			City city= session.get(City.class, 4090);
+				
+			session.delete(city);
+			
+			
+			session.getTransaction().commit();
+			System.out.println("ÞEHÝR SÝLÝNDÝ");
+			
+		}finally {
+			factory.close();
+		}	
+	}
+
+}
