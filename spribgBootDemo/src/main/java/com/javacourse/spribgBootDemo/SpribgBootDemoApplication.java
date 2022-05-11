189c41f882f240dec21a8c4bec8335ac9e969f68
ServerToMbak <server78135@gmail.com>
2022-05-11 20:59:16 +0300

    springBootDemo
    using RestController-GetMapping

---
+++ spribgBootDemo/src/main/java/com/javacourse/spribgBootDemo/SpribgBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javacourse.spribgBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpribgBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpribgBootDemoApplication.class, args);
+	}
+
+}
---
+++ spribgBootDemo/src/test/java/com/javacourse/spribgBootDemo/SpribgBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacourse.spribgBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpribgBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
